1e5e1304087a5ba20c7b1acf829d0d359f4b85eb
delacerdaq <analuizadelacerda@gmail.com>
2024-07-08 18:41:20 -0300

    making the sql file

---
+++ config/letrarium.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE Letrarium;
+
+USE Letrarium;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    terms BOOLEAN NOT NULL DEFAULT 0
+);
