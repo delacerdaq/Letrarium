e241c2228db4d6ea74c9e5727acc77c7d5e727cc
delacerdaq <analuizadelacerda@gmail.com>
2024-07-27 21:04:53 -0300

    adds method to get user profile by id and href for password recovery

--- view/login.php
+++ view/login.php
@@ -1,5 +1,6 @@
 <?php
 require_once '../config/userDAO.php';
+require_once '../controller/ProfileController.php'; 
 session_start();
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
@@ -12,10 +13,19 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if ($user) {
         $_SESSION['user_id'] = $user['id'];
         $_SESSION['username'] = $user['username'];
+
+        // Verifica se o perfil já existe e cria se não existir
+        $profileController = new ProfileController();
+        $profile = $profileController->getProfileByUserId($user['id']);
+        
+        if (empty($profile)) {
+            $profileController->createProfile($user['id']);
+        }
+
         header("Location: user_dashboard.php");
         exit();
     } else {
-        $error_message = "Invalid username or password";
+        $error_message = "Senha ou usuário inválidos.";
     }
 }
 ?>
@@ -53,15 +63,15 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
             <div class="remember-forgot">
                 <label><input type="checkbox" name="remember">Lembre de mim</label>
-                <a href="#">Esqueceu a senha?</a>
+                <a href="reset_password.php">Esqueceu a senha?</a>
             </div>
+
+            <button type="submit" class="btn">Login</button>
 
             <button type="button" class="btn google-btn">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);"><path d="M20.283 10.356h-8.327v3.451h4.792c-.446 2.193-2.313 3.453-4.792 3.453a5.27 5.27 0 0 1-5.279-5.28 5.27 5.27 0 0 1 5.279-5.279c1.259 0 2.397.447 3.29 1.178l2.6-2.599c-1.584-1.381-3.615-2.233-5.89-2.233a8.908 8.908 0 0 0-8.934 8.934 8.907 8.907 0 0 0 8.934 8.934c4.467 0 8.529-3.249 8.529-8.934 0-.528-.081-1.097-.202-1.625z"></path></svg>
             Continuar com o Google
             </button>
-
-            <button type="submit" class="btn">Login</button>
 
             <div class="register-link">
                 <p>Não possui uma conta? <a href="../view/index.php">Cadastre-se aqui!</a></p>
