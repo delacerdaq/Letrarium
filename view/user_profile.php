f6f03a628156423863f0c9f88c841a7f579b6ee2
delacerdaq <analuizadelacerda@gmail.com>
2024-09-15 19:33:28 -0300

    removing unnecessary requires and adding paths to other pages

--- view/user_profile.php
+++ view/user_profile.php
@@ -1,30 +1,27 @@
 <?php
 session_start();
-require_once '../model/Poem.php';
-require_once '../controller/PoemController.php';
+require_once '../controller/poemController.php';
 require_once '../controller/ProfileController.php';
 
 if (!isset($_SESSION['user_id'])) {
-    header("Location: ../view/login.php");
+    header("Location: login.php");
     exit();
 }
 
 $user_id = $_SESSION['user_id'];
 $username = $_SESSION['username'];
 
-// Cria instâncias dos controladores
-$poemController = new PoemController();
 $profileController = new ProfileController();
-
-// Obtém os poemas publicados pelo usuário
-$poems = $poemController->getPoemsByUser($user_id);
-
-// Obtém o perfil atualizado do usuário
 $profile = $profileController->getProfileByUserId($user_id);
 
 // Verifica se o perfil foi encontrado e se há uma foto de perfil
 $profilePicture = !empty($profile['profile_picture']) ? $profile['profile_picture'] : null;
 $bio = !empty($profile['bio']) ? $profile['bio'] : '';
+
+// Obtém os poemas publicados pelo usuário
+$poemController = new PoemController();
+$poems = $poemController->getPoemsByUser($user_id);
+
 ?>
 
 <!DOCTYPE html>
@@ -37,31 +34,37 @@ $bio = !empty($profile['bio']) ? $profile['bio'] : '';
 </head>
 <body>
 
-    <div id="profile-top">
-        <div id="profile-picture">
-            <?php if ($profilePicture): ?>
-                <img src="<?php echo htmlspecialchars($profilePicture); ?>" alt="Profile Picture">
-            <?php else: ?>
-                <div class="placeholder">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24"><path d="M12 2C6.579 2 2 6.579 2 12s4.579 10 10 10 10-4.579 10-10S17.421 2 12 2zm0 5c1.727 0 3 1.272 3 3s-1.273 3-3 3c-1.726 0-3-1.272-3-3s1.274-3 3-3zm-5.106 9.772c.897-1.32 2.393-2.2 4.106-2.2h2c1.714 0 3.209.88 4.106 2.2C15.828 18.14 14.015 19 12 19s-3.828-.86-5.106-2.228z"></path></svg>
-                </div>
-            <?php endif; ?>
-            <a href="edit_profile.php" id="edit-picture">✎</a>
-        </div>
-        <h1><?php echo htmlspecialchars($username); ?></h1>
-        <p><?php echo htmlspecialchars($bio); ?></p> <!-- Exibindo a bio -->
+
+<div id="profile-top">
+
+    <div id="profile-picture">
+    <?php if ($profilePicture): ?>
+        <img src="<?php echo htmlspecialchars($profilePicture); ?>" alt="Profile Picture">
+    <?php else: ?>
+            <div class="placeholder">
+                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24">
+                    <path d="M12 2C6.579 2 2 6.579 2 12s4.579 10 10 10 10-4.579 10-10S17.421 2 12 2zm0 5c1.727 0 3 1.272 3 3s-1.273 3-3 3c-1.726 0-3-1.272-3-3s1.274-3 3-3zm-5.106 9.772c.897-1.32 2.393-2.2 4.106-2.2h2c1.714 0 3.209.88 4.106 2.2C15.828 18.14 14.015 19 12 19s-3.828-.86-5.106-2.228z"></path>
+                </svg>
+            </div>
+        <?php endif; ?>
+        <a href="edit_profile.php" id="edit-picture">✎</a>
     </div>
 
-    <div id="profile-options">
-        <a href="edit_profile.php">Editar Perfil</a>
-        <a href="change_password.php">Mudar Senha</a>
-        <a href="change_email.php">Mudar Email</a>
-        <a href="settings.php">Configurações</a>
-        <a href="preferences.php">Preferências</a>
-        <a href="../view/logout.php">Logout</a>
-    </div>
+    <h1><?php echo htmlspecialchars($username); ?></h1>
+    <p><?php echo htmlspecialchars($bio); ?></p> <!-- Exibindo a bio -->
+</div>
 
-    <div id="poems-section">
+<div id="profile-options">
+    <a href="edit_profile.php">Editar Perfil</a>
+    <a href="reset_password.php">Mudar Senha</a>
+    <a href="change_email.php">Mudar Email</a>
+    <a href="settings.php">Configurações</a>
+    <a href="preferences.php">Preferências</a>
+    <a href="../view/user_dashboard.php">Voltar ao Dashboard</a>
+    <a href="../view/logout.php">Logout</a>
+</div>
+
+<div id="poems-section">
     <h2>Meus Poemas</h2>
     <div class="poems-container">
         <?php if (!empty($poems)): ?>
@@ -70,6 +73,9 @@ $bio = !empty($profile['bio']) ? $profile['bio'] : '';
                     <h3><?php echo htmlspecialchars($poem['title']); ?></h3>
                     <p><?php echo nl2br(htmlspecialchars($poem['content'])); ?></p>
                     <small>Categoria: <?php echo htmlspecialchars($poem['category_name']); ?></small>
+                    <br>
+                    <a href="edit_poem.php?id=<?php echo $poem['id']; ?>">Editar</a> |
+                    <a href="delete_poem.php?id=<?php echo $poem['id']; ?>" onclick="return confirm('Tem certeza que deseja excluir este poema?');">Excluir</a>
                 </div>
             <?php endforeach; ?>
         <?php else: ?>
@@ -79,4 +85,4 @@ $bio = !empty($profile['bio']) ? $profile['bio'] : '';
 </div>
 
 </body>
-</html
+</html>
