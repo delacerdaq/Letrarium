f887f3622952ba1d5e51739e90cc265359c99da4
delacerdaq <analuizadelacerda@gmail.com>
2024-11-26 23:13:38 -0300

    fixing exception for users without profile photo

--- view/user_profile.php
+++ view/user_profile.php
@@ -15,7 +15,7 @@ $profileController = new ProfileController();
 $profile = $profileController->getProfileByUserId($user_id);
 
 // Verifica se o perfil foi encontrado e se há uma foto de perfil
-$profilePicture = !empty($profile['profile_picture']) ? $profile['profile_picture'] : null;
+$profilePicture = (!empty($poem['profile_picture']) && file_exists($poem['profile_picture']));
 $bio = !empty($profile['bio']) ? $profile['bio'] : '';
 
 // Obtém os poemas publicados pelo usuário
@@ -42,7 +42,7 @@ $poems = $poemController->getPoemsByUser($user_id);
         <img src="<?php echo htmlspecialchars($profilePicture); ?>" alt="Profile Picture">
     <?php else: ?>
             <div class="placeholder">
-                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24">
+                <svg xmlns="http://www.w3.org/2000/svg" width="120" height="120" viewBox="0 0 24 24">
                     <path d="M12 2C6.579 2 2 6.579 2 12s4.579 10 10 10 10-4.579 10-10S17.421 2 12 2zm0 5c1.727 0 3 1.272 3 3s-1.273 3-3 3c-1.726 0-3-1.272-3-3s1.274-3 3-3zm-5.106 9.772c.897-1.32 2.393-2.2 4.106-2.2h2c1.714 0 3.209.88 4.106 2.2C15.828 18.14 14.015 19 12 19s-3.828-.86-5.106-2.228z"></path>
                 </svg>
             </div>
